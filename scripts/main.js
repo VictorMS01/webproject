f92279ac58504a65e36585a187389665f333091b
fercassia <feh.cmrg@outlook.com>
2020-10-02 18:14:50 -0300

    Changes

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+   // <!--  
+   var answer = confirm("PARA ACESSAR ESSE CONTEÚDO VOCÊ PRECISA TER MAIS QUE 18 ANOS, OK?");
+   if (!answer){
+   window.location = "BANCO DE DADOS AINDA NAO TEMOS";
+   }
+   //-->
